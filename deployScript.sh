b382f939f1fcdf64bfa8b8e10278f15c0afa1b80
Anitasol <anitaj15@ru.is>
2016-12-13 15:57:35 +0000

    minor changes to deployS docker up

--- deployScript.sh
+++ deployScript.sh
@@ -4,4 +4,4 @@ scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./docker-compose.yaml ec2-user
 scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./.env ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/.env
 scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./run-docker-compose.sh ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/run-docker-compose.sh
 
-ssh -i ~/ToeKey.pem ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com
+ssh -i ~/ToeKey.pem ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com "docker-compose up -d"
