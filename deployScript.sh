b8ba99efccf617fb69646c35ecd5dec85d1f1237
Anitasol <anitaj15@ru.is>
2016-12-13 15:00:40 +0000

    minor changes to deployScript

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./docker-compose.yaml ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/docker-compose.yaml
-scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./.env ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/.env
-scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./run-docker-compose.sh ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/run-docker-compose.sh
+scp -o StrictHostKeyChecking=no -i "ToeKey.pem" ./docker-compose.yaml ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "ToeKey.pem" ./.env ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/.env
+scp -o StrictHostKeyChecking=no -i "ToeKey.pem" ./run-docker-compose.sh ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/run-docker-compose.sh
 
 ssh -i ToeKey.pem ec2-user@35.164.226.192
