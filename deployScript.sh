3c448baa3fb4a1633dfae399e6d36037a35003aa
Anitasol <anitaj15@ru.is>
2016-12-13 16:49:21 +0000

    minor change

--- deployScript.sh
+++ deployScript.sh
@@ -5,4 +5,4 @@ scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./.env ec2-user@ec2-35-164-226
 scp -o StrictHostKeyChecking=no -i "~/ToeKey.pem" ./run-docker-compose.sh ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com:~/run-docker-compose.sh
 
 #run docker-compose up
-ssh -i ~/ToeKey.pem ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com "./run-docker-compose.sh"
+ssh -i ~/ToeKey.pem ec2-user@ec2-35-164-226-192.us-west-2.compute.amazonaws.com "docker-compose up -d"
